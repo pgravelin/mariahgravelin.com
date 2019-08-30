c84038b0fb216575faca4377c10587b05504dd10
Peter Gravelin <pygravelin@gmail.com>
2019-08-29 23:45:39 -0400

    Remove clutter code

--- static/js/custom.js
+++ static/js/custom.js
@@ -9,17 +9,13 @@ $(".grid").imagesLoaded(function() {
    });
 });
 
- // Get the modal
 var modal = $(".modal");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
 var modalImg = $("#modal-img")
 
 $("img").each(function() {
    $(this).on("click", function() {
       modal.css("display", "block");
       modalImg.attr("src", $(this).attr("src"));
-      console.log("WHO");
    });
 });
 
