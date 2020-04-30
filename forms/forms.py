46c9ccebbc9decf5c927137814a13397c1e4e51f
pgravelin <pygravelin@gmail.com>
2020-04-30 14:15:26 -0400

    Reactapp init

--- forms/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import TextField, TextAreaField, SubmitField, validators
-
-class ContactForm(FlaskForm):
-    name = TextField("Name",  [validators.Required()])
-    email = TextField("Email",  [validators.Required(), validators.Email("Please enter your email address.")])
-    subject = TextField("Subject",  [validators.Required()])
-    message = TextAreaField("Message",  [validators.Required()])
